4b48ceb8fb4affd7f612684af585959e15a7f39d
Spencer Kunkel <SSKunkel10sman@gmail.com>
2024-02-05 15:54:36 -0700

    Make pylint happy

--- facial-recognition-client/client.py
+++ facial-recognition-client/client.py
@@ -14,12 +14,19 @@ from client_helper import RapidFaceFollow
 from deepface import DeepFace
 import requests
 
-os.chdir(os.path.dirname(__file__))
-
+# Constants
 CLOCK_TIME = 0.3
 FRAME_GROUP_SIZE = 12
 DB = "data/database"
 SERVER_URL = "http://127.0.0.1:5000/upload-images"
+
+
+def initialize_video_feed():
+    """
+    Initializes the video feed for capturing frames.
+    """
+    feed = RapidFaceFollow()
+    return feed
 
 
 def check_face(frame, send_signals, num):
@@ -61,12 +68,15 @@ def send_images(images):
     encoded_images = []
     for image in images:
         _, buffer = cv2.imencode(".jpg", image)
-        jt = base64.b64encode(buffer).decode()
-        encoded_images.append(jt)
+        encoded_image = base64.b64encode(buffer).decode()
+        encoded_images.append(encoded_image)
     data = json.dumps({"images": encoded_images})
     print("encoding done")
     response = requests.post(
-        SERVER_URL, data=data, headers={"Content-Type": "application/json"}, timeout=None
+        SERVER_URL,
+        data=data,
+        headers={"Content-Type": "application/json"},
+        timeout=None,
     )
     if response.status_code == 200:
         result = response.json()
@@ -75,71 +85,83 @@ def send_images(images):
         print("Error in post:", response.status_code)
 
 
+def process_frame_for_face_recognition(
+    feed, face_mode, frame_group, send_signals, executor
+):
+    """
+    Processes frames from the video feed for face recognition.
+    """
+    try:
+        frame = feed.read()
+    except queue.Empty as e:
+        print(e)
+        return face_mode, frame_group, send_signals
+
+    if face_mode:
+        group_size = len(frame_group)
+        if group_size < FRAME_GROUP_SIZE:
+            frame_group.append(frame)
+        if group_size == 2:
+            print("Starting to check 2nd frame")
+            executor.submit(check_face, frame, send_signals, 2)
+        elif group_size == 5:
+            print("Starting to check 5th frame")
+            executor.submit(check_face, frame, send_signals, 5)
+        elif group_size >= FRAME_GROUP_SIZE:
+            manage_communication_with_server(frame_group, send_signals, executor)
+    else:
+        try:
+            faces = DeepFace.extract_faces(frame, detector_backend="opencv")
+            if len(faces) > 0:
+                print("Starting Face Mode")
+                face_mode = True
+                frame_group.append(frame)
+        except ValueError:
+            pass
+
+    return face_mode, frame_group, send_signals
+
+
+def manage_communication_with_server(frame_group, send_signals, executor):
+    """
+    Manages the communication with the server to send frames after successful face recognition.
+    """
+    if "FINISHED_2" in send_signals and "FINISHED_5" in send_signals:
+        if "MATCHED" in send_signals:
+            print("Matched, sending to server")
+            executor.submit(send_images, frame_group)
+            frame_group.clear()
+            send_signals.clear()
+        elif "NO_MATCH" in send_signals:
+            print("Not matched, restarting")
+            frame_group.clear()
+            send_signals.clear()
+        else:
+            print("WAITING TO FINISH MATCHING")
+
+
 def client():
     """
-    Main function for the client script. Captures video frames, performs face recognition,
-    and sends matched faces to a server.
-
-    This function initializes the video feed, monitors frames for face detection, and manages
-    the flow of capturing, recognizing, and sending frames.
+    Main function for the client script.
     """
-    feed = RapidFaceFollow()
+    feed = initialize_video_feed()
     face_mode = False
     frame_group = []
-    send_signals = []  # FINISHED_2 | FINSIHED_5 | NO_MATCH | MATCHED
+    send_signals = []
     with ThreadPoolExecutor(max_workers=2) as executor:
         print("Starting: Caputuring Frames")
         while True:
             start_time = time.time()
-            try:
-                frame = feed.read()
-            except queue.Empty as e:
-                print(e)
-
-            if face_mode:
-                group_size = len(frame_group)
-                if group_size <= FRAME_GROUP_SIZE:
-                    frame_group.append(frame)
-                if group_size == 2:
-                    print("Starting to check 2nd frame")
-                    executor.submit(check_face, frame, send_signals, 2)
-                elif group_size == 5:
-                    print("Starting to check 5th frame")
-                    executor.submit(check_face, frame, send_signals, 5)
-                elif group_size >= FRAME_GROUP_SIZE:
-                    if "FINISHED_2" in send_signals and "FINISHED_5" in send_signals:
-                        if "MATCHED" in send_signals:
-                            print("Matched, sending to server")
-                            # Send frames to server
-                            executor.submit(send_images, frame_group)
-                            frame_group = []
-                            send_signals = []
-                            face_mode = False
-                        elif "NO_MATCH" in send_signals:
-                            print("Not matched, restarting")
-                            frame_group = []
-                            send_signals = []
-                            face_mode = False
-                    else:
-                        print("WAITING TO FINISH MATCHING")
-
-            else:
-                try:
-                    faces = DeepFace.extract_faces(frame, detector_backend="opencv")
-                    faces = [f for f in faces if f["confidence"] > 7]
-                except ValueError:
-                    pass
-                else:
-                    if len(faces):
-                        print("Starting Face Mode")
-                        face_mode = True
-                        frame_group.append(frame)
+            face_mode, frame_group, send_signals = process_frame_for_face_recognition(
+                feed, face_mode, frame_group, send_signals, executor
+            )
+            manage_communication_with_server(frame_group, send_signals, executor)
 
             left_time = CLOCK_TIME - (time.time() - start_time)
             if left_time > 0:
-                # print("extra time", left_time)
                 time.sleep(left_time)
 
 
 if __name__ == "__main__":
+    os.chdir(os.path.dirname(__file__))
     client()
